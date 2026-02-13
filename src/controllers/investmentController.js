f88962278a06938009c2eb2989fe7a0dd7a0e5c9
fairandjustjames-afk <fairandjustjames@gmail.com>
2026-02-12 23:33:02 -0500

    Create investmentController.js with investment controller functions

---
+++ src/controllers/investmentController.js
@@ -0,0 +1,34 @@
+// investmentController.js
+
+// Function to create an investment
+function createInvestment(req, res) {
+    // Logic to create an investment
+}
+
+// Function to get all investments
+function getAllInvestments(req, res) {
+    // Logic to get all investments
+}
+
+// Function to get a single investment by ID
+function getInvestmentById(req, res) {
+    // Logic to get an investment by ID
+}
+
+// Function to update an investment
+function updateInvestment(req, res) {
+    // Logic to update an investment
+}
+
+// Function to delete an investment
+function deleteInvestment(req, res) {
+    // Logic to delete an investment
+}
+
+module.exports = {
+    createInvestment,
+    getAllInvestments,
+    getInvestmentById,
+    updateInvestment,
+    deleteInvestment
+};
